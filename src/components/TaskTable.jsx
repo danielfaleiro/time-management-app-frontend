a2e8ddcd687d60f815c08852c07543efa40c22c6
Daniel Faleiro <danielfaleirosilva@gmail.com>
2020-09-19 12:27:08 -0300

    TaskTable: sort by date. Colors according to prefered hours.

--- src/components/TaskTable.jsx
+++ src/components/TaskTable.jsx
@@ -1,17 +1,45 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector, connect } from 'react-redux';
+import styled from 'styled-components';
 import { Table, Th, Td } from '../styled-components/html';
 import { getNotes } from '../reducers/notesReducer';
 import DeleteNoteButton from './DeleteNoteButton';
 import EditNoteButton from './EditNoteButton';
 import { userStatus } from '../userStatus';
 
+const StyledTd = styled(Td)`
+  background-color: ${({ meetGoal, isSameUser }) => {
+    if (isSameUser) {
+      return meetGoal ? '#d4ffd4' : '#ffdbdb';
+    }
+    return 'white';
+  }}
+`;
+
 const TaskTable = (props) => {
   const notes = useSelector((state) => state.notes);
-  const token = useSelector((state) => state.user.token);
-  const status = useSelector((state) => state.user.status);
+  const {
+    status, hours, token, id,
+  } = useSelector((state) => state.user);
   const isAdmin = status === userStatus.ADMIN;
+  const dateMap = new Map();
+
+  const checkSameUser = (note) => (isAdmin ? id === note.user.id : true);
+
+  notes.forEach((note) => {
+    if (checkSameUser(note)) {
+      const noteHours = dateMap.get(note.date);
+      dateMap.set(note.date, (noteHours || 0) + note.hours);
+    }
+  });
+
+  const checkMeetGoal = (note) => {
+    if (isAdmin && !checkSameUser(note)) {
+      return dateMap.get(note.date) >= note.user.hours;
+    }
+    return dateMap.get(note.date) >= hours;
+  };
 
   useEffect(() => {
     (async function getNotesFromServer() {
@@ -19,20 +47,27 @@ const TaskTable = (props) => {
     }());
   }, [props, token]);
 
+  const sortedNotes = notes.sort((a, b) => new Date(b.date) - new Date(a.date));
+
   const notesToShow = notes
-    ? notes.map((note) => (
-      <tr key={note.id}>
-        <Td>{note.date}</Td>
-        {isAdmin
-          && <Td>{note.user ? note.user.username : null}</Td>}
-        <Td>{note.task}</Td>
-        <Td>{note.hours}</Td>
-        <Td>
-          <EditNoteButton note={note} />
-          <DeleteNoteButton noteId={note.id} />
-        </Td>
-      </tr>
-    ))
+    ? sortedNotes.map((note) => {
+      const meetGoal = checkMeetGoal(note);
+      const isSameUser = checkSameUser(note);
+      const rowProps = { meetGoal, isSameUser };
+      return (
+        <tr key={note.id}>
+          <StyledTd {...rowProps}>{note.date}</StyledTd>
+          {isAdmin
+          && <StyledTd {...rowProps}>{note.user ? note.user.username : null}</StyledTd>}
+          <StyledTd {...rowProps}>{note.task}</StyledTd>
+          <StyledTd {...rowProps}>{note.hours}</StyledTd>
+          <StyledTd {...rowProps}>
+            <EditNoteButton note={note} />
+            <DeleteNoteButton noteId={note.id} />
+          </StyledTd>
+        </tr>
+      );
+    })
     : undefined;
 
   return (
